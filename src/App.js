ece8fb02d13cd33f885daec923d3a3479c93b772
Shivam Singh <shivamssingh3098@gmail.com>
2022-06-22 22:08:22 +0530

    TicTactoe Game Completed

--- src/App.js
+++ src/App.js
@@ -17,7 +17,30 @@ const reloadeGame = () =>{
   itemArray.fill("empty", 0, 9);
 };
 const checkIsWinner = () =>{
-  //
+  if(itemArray[0]===itemArray[1] && itemArray[0]===itemArray[2] && itemArray[0]!=="empty")
+  {
+ setWinMessage(`${itemArray[0]} wins`)  
+  }
+  else if(itemArray[3]===itemArray[4] && itemArray[3]===itemArray[5] && itemArray[3]!=="empty")
+  {
+    setWinMessage(`${itemArray[3]} wins`)  
+
+  }
+  else if(itemArray[6]===itemArray[7] && itemArray[6]===itemArray[8] && itemArray[6]!=="empty")
+  {
+    setWinMessage(`${itemArray[6]} wins`)  
+
+  }
+  else if(itemArray[0]===itemArray[4] && itemArray[0]===itemArray[8] && itemArray[0]!=="empty")
+  {
+    setWinMessage(`${itemArray[0]} wins`)  
+
+  }
+  else if(itemArray[2]===itemArray[4] && itemArray[2]===itemArray[6] && itemArray[2]!=="empty")
+  {
+    setWinMessage(`${itemArray[2]} wins`)  
+
+  }
 };
 const changeItem=itemNumber => {
   if(winMassage){
@@ -55,7 +78,7 @@ const changeItem=itemNumber => {
 
           <div className="grid">
             {itemArray.map((item, index) => (
-              <Card>
+              <Card color="success" onClick={()=>changeItem(index)}>
                 <CardBody className="box">
                   <Icon name={item} />
                 </CardBody>
